c8bae8fbd6f4c648053da8909144d7ed8468726a
dialop <dianalop1998@gmail.com>
2023-08-10 23:23:41 -0400

    Implemented Sending Messages

--- input.js
+++ input.js
@@ -2,6 +2,11 @@
 
 const { connect } = require('./client');
 
+const cannedMessages = {
+  '1': 'Don\'t hiss me off!',  //When you press key in keyboard "1" msg should appear
+  '2': 'Jussssst Right'
+};
+
 // Initialization of connect variable
 let connection;
 
@@ -38,11 +43,15 @@ const handleUserInput = function(key) {
   if (key === '\u0003') { // Checks if the user has clicked Ctrl + C
     process.exit();  // Terminate game
   }
+
+  const message = cannedMessages[key];
+  if (message) {
+    connection.write(`Say: ${message}`);
+
+  }
   
 };
 
- 
-  
 module.exports = {
   setupInput
 
